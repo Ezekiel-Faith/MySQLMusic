b69ff9dfd0ae2ba7eeb39029093a45d0d3bfa5c2
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-02-14 02:56:04 +0100

    connecting to the music database with JDBC using the DriveManager

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,7 +1,28 @@
 package dev.lpa;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+
 public class Main {
+	private final static String CONN_STRING = "jdbc:mysql://localhost:3306/music";
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		String username = JOptionPane.showInputDialog(
+				null, "Enter DB Username");
+
+		JPasswordField pf = new JPasswordField();
+		int okCxl = JOptionPane.showConfirmDialog(null, pf,
+				"Enter DP Password", JOptionPane.OK_CANCEL_OPTION);
+		final char[] password = (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
+
+		try(Connection connection = DriverManager.getConnection(CONN_STRING, username,
+				String.valueOf(password))) {
+			System.out.println("Success!!! Connection made to the music database");
+			Arrays.fill(password, ' ');
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
